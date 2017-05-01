2e7766b235a7c546edc585e147e92458d966af23
Daniel Timko (RIT Student) <det2948@ad.rit.edu>
2017-05-01 12:34:26 -0400

    Fixed normals in the shader

--- det2948Engine/shaders/fPhong.glsl
+++ det2948Engine/shaders/fPhong.glsl
@@ -1,7 +1,7 @@
 #version 430
 
+in vec3 fragPos;
 in vec3 outNormal;
-in vec3 outPos;
 in vec2 blendedUV;
 
 uniform vec3 camPos;
@@ -10,9 +10,9 @@ uniform vec3 lightPos;
 uniform sampler2D myTexture;
 
 void main() {
-	vec3 N = normalize(outNormal);
-	vec3 L = normalize(lightPos - outPos);
-	vec3 V = normalize(camPos - outPos);
+	vec3 N = outNormal;
+	vec3 L = normalize(lightPos - fragPos);
+	vec3 V = normalize(camPos - fragPos);
 	vec3 H = normalize(V+L);
 
 	float cA = 0.2;
--- det2948Engine/shaders/vPhong.glsl
+++ det2948Engine/shaders/vPhong.glsl
@@ -4,16 +4,16 @@ layout (location = 0) in vec3 position;
 layout (location = 1) in vec2 uv;
 layout (location = 2) in vec3 normal;
 
+out vec3 fragPos;
 out vec3 outNormal;
-out vec3 outPos;
 out vec2 blendedUV;
 
 uniform mat4 camMatrix;
 uniform mat4 modelMatrix;
 
 void main() {
-	gl_Position = camMatrix * modelMatrix * vec4(position, 1);
-	outNormal = normal;
-	outPos = position;
+	gl_Position = camMatrix * modelMatrix * vec4(position, 1.0f);
+	fragPos = vec3(modelMatrix * vec4(position, 1.0f));
+	outNormal = mat3(transpose(inverse(modelMatrix))) * normal;
 	blendedUV = uv;
 }
